e55d566e8b932ddc1a4c15f17c2d934d37add9ed
John Pope <jp@fieldstormapp.com>
2018-09-07 20:35:40 -0400

    Fatal error: 'try!' expression unexpectedly raised an error: Python exception: Traceback (most recent call last):   File "/Users/johndpope/miniconda2/envs/gymai/lib/python2.7/site-packages/tensorflow/python/pywrap_tensorflow.py", line 58, in <module>     from tensorflow.python.pywrap_tensorflow_internal import *   File "/Users/johndpope/miniconda2/envs/gymai/lib/python2.7/site-packages/tensorflow/python/pywrap_tensorflow_internal.py", line 28, in <module>     _pywrap_tensorflow_internal = swig_import_helper()   File "/Users/johndpope/miniconda2/envs/gymai/lib/python2.7/site-packages/tensorflow/python/pywrap_tensorflow_internal.py", line 24, in swig_import_helper     _mod = imp.load_module('_pywrap_tensorflow_internal', fp, pathname, description) ImportError: dlopen(/Users/johndpope/miniconda2/envs/gymai/lib/python2.7/site-packages/tensorflow/python/_pywrap_tensorflow_internal.so, 6): Symbol not found: __ZN10tensorflow17FunctionCallFrame14ConsumeRetvalsEPNSt3__16vectorINS_6TensorENS1_9allocatorIS3_EEEE   Referenced from: /Users/johndpope/miniconda2/envs/gymai/lib/python2.7/site-packages/tensorflow/python/_pywrap_tensorflow_internal.so   Expected in: /Library/Developer/Toolchains/swift-latest/usr/lib/swift/macosx/libtensorflow_framework.so  in /Users/johndpope/miniconda2/envs/gymai/lib/python2.7/site-packages/tensorflow/python/_pywrap_tensorflow_internal.so
    Failed to load the native TensorFlow runtime.
    
    See https://www.tensorflow.org/install/install_sources#common_installation_problems

--- SwiftReinforce/main.swift
+++ SwiftReinforce/main.swift
@@ -10,6 +10,7 @@ import Foundation
 import TensorFlow
 import Python
 
+
 RandomState.global.seed(with: 0)
 
 func runFrozenLake() {
@@ -38,6 +39,80 @@ func runFrozenLake() {
     print(time)
 }
 
+
+func runTensorflowViaPython(){
+    
+    let sys = Python.import("sys")
+    
+
+    dlopen("_pywrap_tensorflow_internal.so", RTLD_NOW | RTLD_GLOBAL)
+    
+    let tf = Python.import("tensorflow")
+    let np = Python.import("numpy")
+
+    let learningRate = 0.01
+    let trainingEpochs = 1000
+    let displayStep = 50
+    
+
+    
+    // Training Data# Train
+    let trainX = np.asarray([3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,7.59,2.167,
+    7.042,10.791,5.313,7.997,5.654,9.27,3.1])
+    let trainY = np.asarray([1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,2.53,1.221,
+    2.827,3.465,1.65,2.904,2.42,2.94,1.3])
+    let n = trainX.shape[0]
+    
+    // tf Graph Input# tf Gr
+    let X = tf.placeholder("float")
+    let Y = tf.placeholder("float")
+    
+    // Set model weights
+    let W = tf.Variable(3, name:"weight")
+    let b = tf.Variable(4, name:"bias")
+    
+    
+    // Construct a linear model# Const
+    let pred = tf.add(tf.multiply(X, W), b)
+    
+    // Mean squared error
+    let cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n)
+    // Gradient descent
+    let optimizer = tf.train.GradientDescentOptimizer(learningRate).minimize(cost)
+    
+    
+    // Initialize the variables (i.e. assign their default value)# Initi
+    let initialize = tf.global_variables_initializer()
+    
+    let sess =  tf.Session()
+    sess.run(initialize)
+    
+    // Fit all training data
+    for epoch in 0..<trainingEpochs{
+        for (x, y) in zip(trainX, trainY){
+            var fd = Dictionary<String,PythonObject.Element>()
+            fd["X"] = x
+            fd["Y"] = y
+            sess.run(optimizer, feed_dict:fd)
+        }
+
+        //Display logs per epoch step
+        if (epoch+1) % displayStep == 0{
+            var fd = Dictionary<String,PythonObject.Element>()
+            fd["X"] = trainX
+            fd["Y"] = trainY
+            let c = sess.run(cost, feed_dict:fd)
+//            print("Epoch:", (epoch+1), "cost=", "{:.9f}".format(c), "W=", sess.run(W), "b=", sess.run(b))
+        }
+    }
+    print ("Optimization Finished!")
+    var fd = Dictionary<String,PythonObject.Element>()
+    fd["X"] = trainX
+    fd["Y"] = trainY
+    let trainingCost = sess.run(cost, feed_dict:fd)
+//    print ("Training cost=", trainingCost, "W=", sess.run(W), "b=", sess.run(b), '\n')
+    
+}
 func runPong() {
    
     let sys = Python.import("sys")
@@ -53,6 +128,9 @@ func runPong() {
     
     let env = gym.make("Pong-v4")
     env.seed(0)
+    
+ 
+    
     let runner = PongRunner(env: env,
                             observationSpace: 80 * 80,
                             actionSpace: 2,
@@ -67,7 +145,8 @@ func runPong() {
 
 func main() {
 //    runFrozenLake()
-    runPong()
+//    runPong()
+    runTensorflowViaPython()
 }
 
 main()
