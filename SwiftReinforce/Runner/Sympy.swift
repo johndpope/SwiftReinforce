e9d6b58460e8e7cccb2d4ebd17796f925bb5cd89
⛳️ <jp+github@fieldstormapp.com>
2018-09-16 13:03:00 -0400

    some progress.

--- SwiftReinforce/Runner/Sympy.swift
+++ SwiftReinforce/Runner/Sympy.swift
@@ -2,20 +2,22 @@ import Foundation
 import Python
 
 
-
 class SymPy{
     
     typealias PO = PythonObject
     static var math:PythonObject!
+    static var np:PythonObject!
     static var sympy:PythonObject!
     static var lambdify:PythonObject!
     static var latex:PythonObject!
+    static var sys:PythonObject!
+    
     // Schema > Run > Pre-actions - source activate gymai
     static func importSysPath(){
-        let sys = Python.import("sys")
+        sys = Python.import("sys")
         let  path = "/Users/\(NSUserName())/miniconda2/envs/gymai/lib/python2.7/site-packages/"
         sys.path.append(path)
-        
+        np = Python.import("numpy")
         math = Python.import("math")
         sympy = Python.import("sympy")
         latex = Python.import("sympy.parsing.latex")
@@ -25,6 +27,8 @@ class SymPy{
     //  https://github.com/fsamija/ethicon/blob/479b7c916d0fc97d05ff94a1694fbc13a7f1a9c3/venvs/edxapp/lib/python2.7/site-packages/sympy/utilities/lambdify.py#L121
     // the lambdify function breaks out of the box - https://github.com/tensorflow/swift/issues/72
     // use lambdastr to get around this - TODO - work out how to eval it.
+    
+    //Returns a string that can be evaluated to a lambda function.
     static func testLambdifyStr(){
         
         let a = sympy.DeferredVector("a")
@@ -37,11 +41,53 @@ class SymPy{
         print("func:",funcArray)
         
         let strTest = lambdify.lambdastr([a,s],funcArray[0])
-        print("lambdastr:",strTest)//lambdastr: lambda a,x: (-(a[0]*x[0] + a[1]*x[1] + a[2]*x[2])/(x[0]**2 + x[1]**2 + x[2]**2))
-        // how to eval this function??
-//        let lambdaStr = lambdify.lambdastr(sympy.Pow(x,2) ,[x]) //(x**2, [x])
-//        let x = Python.eval(strTest)
-//        print("x:",x)
+        print("λ str:",strTest)//lambdastr: lambda a,x: (-(a[0]*x[0] + a[1]*x[1] + a[2]*x[2])/(x[0]**2 + x[1]**2 + x[2]**2))
+
+        let λ = Python.eval(strTest,["builtins":"None"])
+        print("λ fn:",λ)
+//        let xArray = np.array(1)
+//        let aArray = np.array(1,1)
+//
+//        let e0 = Python.map(λ,xArray,aArray)
+        
+//        print("λ(1,2):",λ(xArray,aArray))
+    }
+    
+    static func simpleLambdify(){
+        //>>> f = lambdify((x,y,z), [z,y,x])
+          //  >>> f(1,2,3)
+        
+          let x = sympy.Symbol("x")
+          let y = sympy.Symbol("y")
+          let z = sympy.Symbol("z")
+          let fn =  sympy.Function("f")
+        
+//        class my_func:PythonObject{
+//            func eval(){
+//                print("ok")
+//            }
+//
+//        }
+        
+        /* TODO - hook up this function
+ class my_func(Function):
+ ...
+ ...     @classmethod
+ ...     def eval(cls, x):
+ ...         if x.is_Number:
+ ...             if x is S.Zero:
+ ...                 return S.One
+ ...             elif x is S.Infinity:
+ ...                 return S.Zero
+ ...
+ ...     def _eval_is_real(self):
+ ...         return self.args[0].is_real
+ */
+//         let strTest = lambdify.lambdastr([x,y,z],my_func)
+        // eval it
+        // exec
+        
+        
     }
     
     static func testTupleDeclaration(){
@@ -77,6 +123,7 @@ class SymPy{
     }
     
     static func testBackProp(){
+        print("---------------------------")
         // back propagation with sympy < 125 lines of code
        //  https://github.com/Sheyne/Neural/blob/1b0563a65480448a48b395f74a70a7d949112f42/sympy_test.py#L104
         let y = sympy.Symbol("y")
@@ -86,23 +133,38 @@ class SymPy{
         let cost =  sympy.Pow((y - t),2) / 2
         let args = sympy.Array([y, t])
 
-        let costFunc = lambdify.lambdastr(args, cost) // this blows up - : Python exception: 'NoneType' object has no attribute 'f_locals'
-        //https://groups.google.com/forum/#!topic/sympy/vQACccLGxsk
+        let costFunc = lambdify.lambdastr(args, cost)
+        print("costFunc:",costFunc)
+
         let costP = sympy.diff(cost, y)
+        print("costP:",costP)
+        
          let costPf = lambdify.lambdastr([y, t],costP)
-//        let costPf = sympy.lambdify([y, t], costP)
+//        let costPf = sympy.lambdify([y, t], costP) // this blows up  https://github.com/tensorflow/swift/issues/72
         print("costPF:",costP)
+        
     }
     
+    
+    //see lambdify python class
+    // https://github.com/fsamija/ethicon/blob/479b7c916d0fc97d05ff94a1694fbc13a7f1a9c3/venvs/edxapp-sandbox/lib/python2.7/site-packages/sympy/utilities/lambdify.py#L171
     static func lambdaSinTest(){
         let abc = Python.import("sympy.abc")
 //        let x = abc.x()
         let y = sympy.abc.y()
         let z = sympy.abc.z()
-
         let x = sympy.Symbol("x")
         print("x:",x)
 
+        /*
+         Use own dictionaries:
+         >> def my_cool_function(x): ...
+         >> dic = {"sin" : my_cool_function}
+         >> f = lambdify(x, sin(x), dic)
+         Now f would look like:
+         >> lambda x: my_cool_function(x)
+         */
+        // can we pass in swift functions??
         func my_cool_function(){
 
         }
@@ -125,9 +187,8 @@ class SymPy{
 
         solveEquation()
         testLambdifyStr()
-        testBackProp()
         lambdaSinTest()
-     
+        testBackProp()
 
     }
 }
