91bdd29919e61ad7c8101538ad6027b5a1943e19
⛳️ <jp+github@fieldstormapp.com>
2018-09-15 21:13:01 -0400

    updates

--- SwiftReinforce/Runner/Sympy.swift
+++ SwiftReinforce/Runner/Sympy.swift
@@ -18,23 +18,49 @@ class SymPy{
     static func run(){
         importSysPath()
         
-        let sympy = Python.import("sympy")
+        let sp = Python.import("sympy")
         let latex = Python.import("sympy.parsing.latex")
-        let expr = latex.parse_latex("x = \\frac{-b \\pm \\sqrt{b^2-4ac}}{2a}")
+        
+        // declare sympy vars by tuple or individually
+        let tuple = sp.symbols("a,b,c,x",real:true)
+        print("a:",tuple[0])
+        
+//        let a = sp.Symbol("a")
+//        let b = sp.Symbol("b")
+//        let c = sp.Symbol("c")
+//        let x = sp.Symbol("x")
+
+        
+        // Solve equation passing variable values
+        let exp = sp.S("(a+b)*40-(c-a)/0.5")
+         print("exp:",exp)
+        var test = exp.evalf(subs:["a":6, "b":5, "c":2])
+        print("test:",test)
+        
+        // latex 1st sample from https://github.com/kostub/iosMath
+        let str = "\\frac{-b  \\sqrt{b^2-4ac}}{2a}"
+        let expr = latex.parse_latex(str)
+        sp.init_printing(pretty_print:true,use_latex:true)
+
         print("expr:",expr)
-        let np = Python.import("numpy")
-        //let mpmath = Python.import("mpmath")
+        test = expr.evalf(subs:["a":1, "b":1, "c":1]) //,"pm":1 -> plus / minus https://github.com/sympy/sympy/issues/5305
+        print("test:",test)
+
         
-        let t = sympy.Symbol("t")
-        let y = sympy.Symbol("y")
         
-        let sigmoid = 1/(1+sympy.exp(-y))
-        let cross_entropy = y * sympy.ln(t)+(1 - y) * sympy.ln(1 - t)
-        let cost =  sympy.Pow((y - t),2) / 2
-        let costFunc = sympy.lambdify([y, t], cost)
-        let costP = sympy.diff(cost, y)
-        let costPf = sympy.lambdify([y, t], costP)
-        print("costPF:",costP)
+        // back propagation with sympy < 125 lines of code
+        // https://github.com/Sheyne/Neural/blob/1b0563a65480448a48b395f74a70a7d949112f42/sympy_test.py#L104
+        let y = sp.Symbol("y")
+        let t = sp.Symbol("t")
+        let sigmoid = 1/(1+sp.exp(-y))
+        let cross_entropy = y * sp.ln(t)+(1 - y) * sp.ln(1 - t)
+        let cost =  sp.Pow((y - t),sp.Integer(2)) / sp.Integer(2)
+        let args = sp.Array([y, t])
+        let costFunc = sp.lambdify(args, cost,modules:"numpy") // this blows up - : Python exception: 'NoneType' object has no attribute 'f_locals'
+        //https://groups.google.com/forum/#!topic/sympy/vQACccLGxsk
+        let costP = sp.diff(cost, y)
+//        let costPf = sp.lambdify([y, t], costP)
+//        print("costPF:",costP)
 
     }
 }
